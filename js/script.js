7c43bd3dc39c1bd427d74963b3f32b733b79a086
Esteban Duran <esteban321duran@gmail.com>
2020-11-12 01:26:28 -0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 'use strict';
-// const boardSquares = document.querySelectorAll(".board-square");
-// let boardSquarePiece;
-
-// for (let i = 0; i < boardSquares.length; i++) {
-//   boardSquarePiece = boardSquares[i].firstElementChild;
-//   console.log(boardSquarePiece);
-//   boardSquarePiece.src = "../assets/SVGWithShadow/b_rook_svg_withShadow.svg";
-// }
 
 const initializeBoard = function () {
   const boardSquares = document.querySelectorAll('.board-square');
@@ -253,27 +245,6 @@ const renderAllPieces = function () {
   draw(drawInfo); // assign sprites to the src attribute of the boardSquares if there's a piece on them
   cleanup(cleanupInfo); //add none to the classList of any empty boardSquare
 };
-
-// const getPiecesDrawInfo = function (cleanupInfo) {
-//   let p = 0;
-//   let drawInfo = [];
-//   for (let y = 0; y < ROWS; y++) {
-//     for (let x = 0; x < COLUMNS; x++) {
-//       if (!pieces[p].alive) {
-//         p++;
-//         cleanupInfo.push([x, y]);
-//         continue;
-//       }
-//       if (x === pieces[p].x && y === pieces[p].y) {
-//         drawInfo.push([x, y, pieces[p].sprite]);
-//         p++;
-//       } else {
-//         cleanupInfo.push([x, y]);
-//       }
-//     }
-//   }
-//   return drawInfo;
-// };
 const getPiecesDrawInfo = function (cleanupInfo) {
   let piece;
   let pieceAtPos = false;
@@ -322,22 +293,6 @@ const cleanup = function (cleanupInfo) {
       squareBoardImg.classList.add('none');
   }
 };
-
-// const getCleanupInfo = function (drawInfo) {
-// DONE
-// basically this bidimensional array contains all the pairs of coodinates that weren't in the drawInfo.
-//   // A way of implementing this could be to fill cleanupInfo with all possible pairs of coordinates and then remove those who are present in the draw info
-//   const cleanupInfo = [[0, 0]];
-//   let d = 0;
-//   for (let y = 0; y < ROWS; y++) {
-//     for (let x = 0; x < COLUMNS; x++) {
-//       if (x !== drawInfo[d][0] && y !== drawInfo[d][1]) {
-//         cleanupInfo.push([x, y]);
-//         d++;
-//       }
-//     }
-//   }
-// };
 
 //EVENT_HANDLER
 const boardSquareClicked = function (e) {
@@ -391,29 +346,6 @@ const selectPiece = function (clickedPiece) {
 const setSelected = function (clickedPiece) {
   clickedPiece.selected = true;
 };
-
-// calcAvailableMovesColor = function (color) {
-//   for (let i = 0; i < pieces.length; i++) {
-//     if (pieces[i].color === color && pieces[i].isAlive) {
-//       switch (pieces[i].type) {
-//         case 'rook':
-//           calcRookMoves(color, pieces[i]);
-//           break;
-//         case 'knight':
-//           break;
-//         case 'bishop':
-//           break;
-//         case 'queen':
-//           break;
-//         case 'king':
-//           break;
-//         case 'pawn':
-//           break;
-//       }
-//     }
-//   }
-// };
-
 const renderAvailableMoves = function (moves) {
   //TODO
   let cleanupInfo = [];
@@ -467,8 +399,6 @@ const drawBackground = function (drawInfo) {
   }
 };
 
-// it appears that wherever was a friendly piece before dying, and you can now move above, it's unable to resotre the previous background-image
-//DONE by only allowing the recolection of drawInfo up to how many noves there where to draw, i wasn't allowing the cleanupArray to be fully constructed
 const cleanupBackground = function (cleanupInfo) {
   let squareBoardBackground;
   for (let i = 0; i < cleanupInfo.length; i++) {
@@ -658,63 +588,4 @@ let alivePieces = 32;
 let activePlayer = 'white';
 
 subscribeEventListeners();
-
 renderAllPieces();
-// pieces[24].x = 4;
-pieces[24].y = 4; //DONE //changing these properties kills half of the white pieces because when i look for the drawInfo of the pieces, i only look for each piece once by their order of appearance on the matrix. perhaps a way to solve this would be to look for the pieces drawInfo by some sort of id or by directly referencing them on the array
-pieces[24].x = 3;
-pieces[24].moves = calcRookMoves(pieces[24]);
-pieces[25].x = 5;
-pieces[25].y = 0;
-pieces[25].moves = calcKnightMoves(pieces[25]);
-
-pieces[13].alive = false;
-pieces[0].alive = false;
-pieces[5].alive = false;
-renderAllPieces();
-calcRookMoves(pieces[31]);
-
-//DONE
-// a way to solve this problem would be to store the row and column of each board square element in its class or id that way we coul try to locate it in our board matrix.
-// The board matrix could maybe store only the board square elements that way i could use the includes() array method
-
-// const numbers = [
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 1],
-// ];
-
-// console.log(numbers[7].indexOf(1)); returns 7
-
-// a structure like this and board square id's could allow me to identify exactly which one fired the event
-
-//Attempt to solve the current existing bug//DONE
-// while (p < pieces.length) {
-//   y = 0;
-//   x = 0;
-//   p = 0;
-//   b = false;
-//   while (y < ROWS && !b) {
-//     while (x < COLUMNS && !b) {
-//       if (!pieces[p].alive) {
-//         p++;
-//         cleanupInfo.push([x, y]);
-//         b = true;
-//       }
-//       if (x === pieces[p].x && y === pieces[p].y) {
-//         drawInfo.push([x, y, pieces[p].sprite]);
-//         p++;
-//         b = true;
-//       } else {
-//         cleanupInfo.push([x, y]);
-//       }
-//       x++;
-//     }
-//     y++;
-//   }
-// }
